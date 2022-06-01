48be39c018c5848c7e5e9ab943755eeff6c4dabb
Harshal Patil <86585860+Harsh23patil@users.noreply.github.com>
2022-06-01 15:45:33 +0530

    changing Location of image

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let button = document.getElementById('btn');
 var r1 = Math.floor(Math.random()*6) + 1;
 var r2 = Math.floor(Math.random()*6) + 1;
 button.addEventListener('click', () => {
-    document.querySelector(".d1 img").setAttribute("src","Image/dice"+r1+".png");
-    document.querySelector(".d2 img").setAttribute("src","Image/dice"+r2+".png");
+    document.querySelector(".d1 img").setAttribute("src","dice"+r1+".png");
+    document.querySelector(".d2 img").setAttribute("src","dice"+r2+".png");
     if(r1 > r2){
         document.querySelector(".output").innerHTML = "Player 1 Win";
     }else if(r1 < r2){
