10c9e67ffa905f18ee668af3f1174636c31d9daa
Harshal Patil <86585860+Harsh23patil@users.noreply.github.com>
2022-06-01 15:43:47 +0530

    Adding Files

---
+++ index.js
@@ -0,0 +1,14 @@
+let button = document.getElementById('btn');
+var r1 = Math.floor(Math.random()*6) + 1;
+var r2 = Math.floor(Math.random()*6) + 1;
+button.addEventListener('click', () => {
+    document.querySelector(".d1 img").setAttribute("src","Image/dice"+r1+".png");
+    document.querySelector(".d2 img").setAttribute("src","Image/dice"+r2+".png");
+    if(r1 > r2){
+        document.querySelector(".output").innerHTML = "Player 1 Win";
+    }else if(r1 < r2){
+        document.querySelector(".output").innerHTML = "Player 2 Win";
+    }else{
+        document.querySelector(".output").innerHTML = "This is Tye";
+    }
+});
